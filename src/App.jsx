b48c06d971ea1696b84c28f2212d587322810db7
Mohd Hassan <mohdhassana04@gmail.com>
2025-01-16 21:20:05 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+import './App.css'
+import Header from './components/header'
+import Hero from './components/hero'
+
+
+function App() {
+ 
+  return (
+   
+      <div>
+
+      <Header />
+      <Hero />
+       </div>
+  )   
+}
+
+export default App
